4f64459b7cdd45734986e913ff6b8c8bb7f88b8f
Hong Mingyeong <rally20010105@gmail.com>
2024-07-19 13:12:02 +0900

    가위바위보(2)

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ imgArray[0] = "images/보.png";
 imgArray[1] = "images/바위.png";
 imgArray[2] = "images/가위.png";
 
+let a;
+
 function imagetime() {
   a = Math.floor(Math.random() * 3);
   img.src = imgArray[a];
@@ -20,22 +22,21 @@ function imagetime() {
 
 setInterval(imagetime, 120);
 
-let a;
 let userscore = 0;
 let computerscore = 0;
 
-//가위를 클릭한 경우
+// 가위를 클릭한 경우
 scissors.onclick = () => {
   clearInterval(); // setInterval() 호출을 취소
-  //승리하는 경우(컴퓨터 : 보를 낸 경우 )
+  // 승리하는 경우(컴퓨터 : 보를 낸 경우 )
   if (a == 0) {
     alert("승리");
     userscore++;
     user_score.textContent = userscore;
   } else if (a == 2) {
-    //(컴퓨터 : 가위 낸 경우)
+    // (컴퓨터 : 가위 낸 경우)
     alert("무승부");
-  } //(컴퓨터 : 묵 낸 경우)
+  } // (컴퓨터 : 묵 낸 경우)
   else {
     alert("패배");
     computerscore++;
@@ -43,7 +44,7 @@ scissors.onclick = () => {
   }
 };
 
-//바위를 클릭한 경우
+// 바위를 클릭한 경우
 rock.onclick = () => {
   clearInterval();
   if (a == 0) {
@@ -52,27 +53,27 @@ rock.onclick = () => {
     computerscore++;
     computer_score.textContent = computerscore;
   } else if (a == 2) {
-    //(컴퓨터 : 가위낸 경우)
+    // (컴퓨터 : 가위낸 경우)
     alert("승리");
     userscore++;
     user_score.textContent = userscore;
-  } //(컴퓨터 : 묵 낸경우)
+  } // (컴퓨터 : 묵 낸경우)
   else {
     alert("무승부");
   }
 };
-//보를 클릭한 경우
+// 보를 클릭한 경우
 paper.onclick = () => {
   clearInterval();
   if (a == 0) {
     // (컴퓨터 : 보를 낸 경우)
     alert("무승부");
   } else if (a == 2) {
-    //(컴퓨터 : 가위낸 경우)
+    // (컴퓨터 : 가위낸 경우)
     alert("패배");
     computerscore++;
     computer_score.textContent = computerscore;
-  } //(컴퓨터 : 묵 낸경우)
+  } // (컴퓨터 : 묵 낸경우)
   else {
     alert("승리");
     userscore++;
@@ -80,7 +81,7 @@ paper.onclick = () => {
   }
 };
 
-//다시 시작하기 클릭한 경우
+// 다시시작 클릭한 경우
 reset_btn.onclick = () => {
   let check = confirm("다시 시작하시겠습니까?");
   if (check == true) {
@@ -88,7 +89,7 @@ reset_btn.onclick = () => {
   }
 };
 
-//게임종료 클릭한 경우
+// 게임종료 클릭한 경우
 end_btn.onclick = () => {
   let check = confirm("게임을 종료하시겠습니까?");
   if (check == true) {
